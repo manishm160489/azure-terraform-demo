935272b03c4e282f78b46dfcf95e7f2aacf26ff0
manishm160489 <manishm.164@outlook.com>
2025-09-06 13:38:09 +0000

    Add Terraform pipeline workflow

--- infra/main.tf
+++ infra/main.tf
@@ -23,6 +23,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-demo1"
   location = var.location
 }
+variable "storage_account_name" {
+  type        = string
+  description = "Name of the storage account"
+}
+
 # main.tf
 resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
